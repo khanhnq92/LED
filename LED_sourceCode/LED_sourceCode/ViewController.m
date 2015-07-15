e3fe9bd05dfb1d0464823401a44d7bb9838884a4
Huy Quang Ngo <manhle@Huys-Mac-mini.local>
2015-07-15 15:09:48 +0700

    New Project

---
+++ LED_sourceCode/LED_sourceCode/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LED_sourceCode
+//
+//  Created by Huy Quang Ngo on 7/15/15.
+//  Copyright (c) 2015 Khanh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
