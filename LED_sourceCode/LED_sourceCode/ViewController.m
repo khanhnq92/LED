c68214aa223dde35649e9c82cb55bebf3496d68d
Huy Quang Ngo <manhle@Huys-Mac-mini.fss.com.vn>
2015-07-20 13:39:23 +0700

    Chay nguoc

--- LED_sourceCode/LED_sourceCode/ViewController.m
+++ LED_sourceCode/LED_sourceCode/ViewController.m
@@ -12,16 +12,79 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    CGFloat magin;
+    int _numberOfBall;
+    CGFloat space;
+    CGFloat balldiameter;
+    NSTimer* timer;
+    int indexBall;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    magin=40.0;
+    balldiameter=24.0;
+    _numberOfBall=8;
+//    [self placeBallatX:100 andY:100 withTag:1];
+    [self numberOfBall];
+    [self drawRowofBall:_numberOfBall];
+    indexBall=_numberOfBall+1;
+    timer=[NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(runningBall) userInfo:nil repeats:true];
+}
+-(void)runningBall{
+    if(indexBall!=_numberOfBall+1){
+        [self turnOff:indexBall];
+    }
+    indexBall--;
+    if(indexBall==-1)
+        indexBall=_numberOfBall-1;
+    [self turnOn:indexBall];
+}
+-(void)turnOn:(int)tagBall{
+  UIView* vBall=[self.view viewWithTag:tagBall+1];
+    if(vBall&&[vBall isMemberOfClass:[UIImageView class]]){
+        UIImageView* img=(UIImageView*)vBall;
+        img.image=[UIImage imageNamed:@"ball_running"];
+    }
+}
+-(void)turnOff:(int)tagBall{
+    UIView* vBall=[self.view viewWithTag:tagBall+1];
+    if(vBall&&[vBall isMemberOfClass:[UIImageView class]]){
+        UIImageView* img=(UIImageView*)vBall;
+        img.image=[UIImage imageNamed:@"ball"];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) placeBallatX:(CGFloat)x
+                andY:(CGFloat)y
+             withTag:(int)tag{
+    UIImageView*ball=[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ball"]];
+    ball.center=CGPointMake(x,y);
+    ball.tag=tag;
+    [self.view addSubview:ball];
 }
-
+-(CGFloat) spaceBetweenBall:(int)n{
+    return (self.view.bounds.size.width -2*magin)/(n-1);
+}
+-(void)numberOfBall{
+    bool stop=false;
+    int n=3;
+    while(!stop){
+        space=[self spaceBetweenBall:n];
+        if(space<balldiameter)
+            stop=true;
+        else
+            NSLog(@"Number of ball %d, space between ball center %3.0f",n,space);
+        n++;
+    }
+}
+-(void) drawRowofBall:(int)numberBall{
+    space=[self spaceBetweenBall:numberBall];
+    for (int i=0; i<numberBall; i++) {
+        [self placeBallatX:magin+i*space
+                      andY:150
+                   withTag:i+1];
+    }
+}
 @end
